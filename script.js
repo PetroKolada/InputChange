ababeec0038e275eefc09ffa4d725f1c0af1dc7d
PetroKolada <75851079+PetroKolada@users.noreply.github.com>
2024-01-15 16:44:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const myForm = document.querySelector("#myForm")
+const sendButton = myForm.querySelector('#sendButton')
+
+sendButton.addEventListener("click", () => {
+    console.log("func");
+})
